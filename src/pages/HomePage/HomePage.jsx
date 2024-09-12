3d93e0aabd780ae4fa622884429aafec9600773a
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-09-11 20:16:11 -0700

    styles

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 
-
+import './HomePage.css'
 import { NavLink } from "react-router-dom";
 import smashBurgerLogo from '../../images/browser/smashBurgerLogo.jpg'
 import homeLogo from '../../images/browser/homeLogo.jpg'
@@ -10,63 +10,24 @@ import storeLogo from '../../images/browser/storeLogo.jpg'
 
 export default function HomePage() {
   return (
-    <div style={{
-      width: '100%',
-      height: 'auto',
-      backgroundColor: 'lightblue'
-    }}>
-    
-    <div style={{
-      display: 'inline-flex',
-      width: '100%'
-    }}> 
-    <NavLink to='/'>
-    <img src={smashBurgerLogo} />
-    </NavLink>
+    <div className="homePageMainBox">
+  
+    <div className="homePageNavBarMainBox"> 
+    <NavLink to='/'><img src={smashBurgerLogo} /></NavLink>
 
-    <div style={{
-      display: 'inline-flex',
-      width: '100%',
-      justifyContent: 'space-around',
-      alignItems: 'center'
-    }}>
-
-    <NavLink to='/'>
-    <img src={homeLogo} />
-    </NavLink>  
-    <NavLink to='/menu'>
-    <img src={menuLogo} />
-    </NavLink>
-    <NavLink to='/media'>
-    <img src={smashLogo} />
-    </NavLink>
-    <NavLink to='/store'>
-    <img src={storeLogo} />
-    </NavLink>
+    <div className="homePageNavBarSubBox">
+    <NavLink to='/'><img src={homeLogo} /></NavLink>  
+    <NavLink to='/menu'><img src={menuLogo} /></NavLink>
+    <NavLink to='/media'><img src={smashLogo} /></NavLink>
+    <NavLink to='/store'><img src={storeLogo} /></NavLink>
     </div>
-    
     </div>
 
-    <div style={{
-      padding: '5%',
-     
-    }}>
-      <div style={{
-        width: '100%',
-        height: '40vh',
-        border: '10px dodgerblue solid'
-      }}>
+    <div className='homePageSectionOuterBox'>
+    <div className='homePageSectionSubBox'>
         home page contents
-      </div>
     </div>
-    
-
-
-
-
-
-
-
+    </div>
 
     </div>
   )
